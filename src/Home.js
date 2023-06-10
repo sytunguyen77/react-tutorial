111f8ab4cfe676ec19be5bf36a9bf2d0559f775d
Sy Tu Nguyen <54698578+sytunguyen77@users.noreply.github.com>
2023-06-10 03:11:42 -0500

    first commit

--- src/Home.js
+++ src/Home.js
@@ -17,14 +17,11 @@ const Home = () => {
 
    useEffect(() => {
       console.log("use effect ran");
-      console.log(name);
-   }, [name]);
+   }, []);
 
    return (
       <div className="home">
          <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
-         <button onClick={() => setName("luigi")}>change name</button>
-         <p>{name}</p>
       </div>
    );
 };
