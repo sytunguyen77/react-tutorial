794cd706070e4c3029165c813b3cb886a1a9302f
Sy Tu Nguyen <54698578+sytunguyen77@users.noreply.github.com>
2023-06-10 11:34:46 -0500

    first commit

--- src/Home.js
+++ src/Home.js
@@ -3,19 +3,37 @@ import BlogList from "./BlogList";
 
 const Home = () => {
    const [blogs, setBlogs] = useState(null);
+   const [isPending, setIsPending] = useState(true);
+   const [error, setError] = useState(null);
 
    useEffect(() => {
-      fetch("http://localhost:8000/blogs")
-         .then((res) => {
-            return res.json();
-         })
-         .then((data) => {
-            console.log(data);
-            setBlogs(data);
-         });
+      setTimeout(() => {
+         fetch("http://localhost:8000/blogs")
+            .then((res) => {
+               if (!res.ok) {
+                  throw Error("could not fetch the data for that resource");
+               }
+               return res.json();
+            })
+            .then((data) => {
+               setBlogs(data);
+               setIsPending(false);
+               setError(null);
+            })
+            .catch((err) => {
+               setIsPending(false);
+               setError(err.message);
+            });
+      }, 1000);
    }, []);
 
-   return <div className="home">{blogs && <BlogList blogs={blogs} title="All Blogs!" />}</div>;
+   return (
+      <div className="home">
+         {error && <div>{error} </div>}
+         {isPending && <div>Loading...</div>}
+         {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+      </div>
+   );
 };
 
 export default Home;
