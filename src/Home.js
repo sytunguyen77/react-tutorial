c3e31ec85fe36e81ed3bf74b18224474f3fbb626
Sy Tu Nguyen <54698578+sytunguyen77@users.noreply.github.com>
2023-06-10 03:22:04 -0500

    first commit

--- src/Home.js
+++ src/Home.js
@@ -2,28 +2,20 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-   const [blogs, setBlogs] = useState([
-      { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-      { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-      { title: "Web dev top tips", body: "lorem ipsum...", author: "mario", id: 3 },
-   ]);
-
-   const [name, setName] = useState("mario");
-
-   const handleDelete = (id) => {
-      const newBlogs = blogs.filter((blog) => blog.id !== id);
-      setBlogs(newBlogs);
-   };
+   const [blogs, setBlogs] = useState(null);
 
    useEffect(() => {
-      console.log("use effect ran");
+      fetch("http://localhost:8000/blogs")
+         .then((res) => {
+            return res.json();
+         })
+         .then((data) => {
+            console.log(data);
+            setBlogs(data);
+         });
    }, []);
 
-   return (
-      <div className="home">
-         <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
-      </div>
-   );
+   return <div className="home">{blogs && <BlogList blogs={blogs} title="All Blogs!" />}</div>;
 };
 
 export default Home;
